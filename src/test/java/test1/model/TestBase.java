177961ee20671a7fe20f4ca597a9413b60b8b79d
Andrey <terehovandrej@gmail.com>
2019-05-02 19:24:49 +0300

    Оптимизация кроссбраузерности

--- src/test/java/test1/model/TestBase.java
+++ src/test/java/test1/model/TestBase.java
@@ -1,12 +1,13 @@
 package test1.model;
 
+import org.openqa.selenium.remote.BrowserType;
 import test1.appmanager.ApplicationManager;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeClass(alwaysRun = true)
     public void SetUp() {
