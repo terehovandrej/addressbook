8b8273a9713729df09843405f61faaf2a108619a
Andrey <terehovandrej@gmail.com>
2019-05-03 13:35:10 +0300

    добавлены комментарии

--- src/test/java/test1/appmanager/HelperBase.java
+++ src/test/java/test1/appmanager/HelperBase.java
@@ -16,9 +16,14 @@ public class HelperBase {
 
     protected void type(By locator, String text) {
         click(locator);
+        //заполнение дефолтными значениями, если переданный текст равен null
         if (text != null) {
-            driver.findElement(locator).clear();
-            driver.findElement(locator).sendKeys(text);
+            String existingText = driver.findElement(locator).getAttribute("value");
+            //если поля уже заполнены переданными значениями, то повторно не заполняются
+            if (! text.equals(existingText)) {
+                driver.findElement(locator).clear();
+                driver.findElement(locator).sendKeys(text);
+            }
         }
     }
 }
